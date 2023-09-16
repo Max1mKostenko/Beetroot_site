e3c13afd6c1a76cf67355125db4853615c73c4e2
Maxim <maxim2k18gh8hg@gmail.com>
2023-09-16 10:17:22 +0300

    upd hw_2

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
+    path("", views.main, name="main"),
+    path("about", views.about, name="about"),
+    path("contacts", views.contacts, name="contacts"),
+    path("test_1", views.test_1, name="test_1"),
+    path("test_2", views.test_2, name="test_2"),
+    path("test_3", views.test_3, name="test_3"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "myapp/index.html")
+def main(request):
+    data = {'title': 'Main Page',
+            'values': ["Main Page", "About Page", "Contact Page"],
+            "dictionary": {'car': 'BMW',
+                           'model': 'X5',
+                           'age': '2018'
+                           }
+            }
+    return render(request, "myapp/main.html", data)
 
 
 def about(request):
     return render(request, "myapp/about.html")
+
+
+def contacts(request):
+    return render(request, "myapp/contacts.html")
+
+
+def test_1(request):
+    return render(request, "myapp/test_1.html")
+
+
+def test_2(request):
+    return render(request, "myapp/test_2.html")
+
+
+def test_3(request):
+    return render(request, "myapp/test_3.html")
