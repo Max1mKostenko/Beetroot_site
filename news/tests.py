f1379aefead64adf6ea61ab38910ca6f4e0ec08b
Maxim <maxim2k18gh8hg@gmail.com>
2023-09-29 20:38:28 +0300

    upd unit test

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Articles
 
-# Create your tests here.
+
+class YourTestClass(TestCase):
+    def test_false_is_false(self):
+        response = Articles.title
+        self.assertEqual(response, "test data")
+
+"""
+Знаю що не правильно, але я тупо не знаю як відтестувати якісь данні 
+"""
